0b3a9501747d82c9ff751c0b15063e9edfdb9d33
Bruno Dahlem <bruno.dahlem2@gmail.com>
2022-12-15 14:57:10 +0100

    Added sound

--- script.js
+++ script.js
@@ -3,20 +3,38 @@ const countdownDiv = document.getElementById('countdown');
 let goBtn = document.getElementById("goBtn");
 let inputCountdown = document.getElementById('numberParam');
 let tempsRestant = '';
+let audioBip = new Audio("./media/bip.mp3");
+let audioBuzzEnd = new Audio("./media/buzzEnd.mp3");
 
 goBtn.addEventListener('click', function () {
+    clearInterval(compteurInterval);
     launchCountdown();
 });
 
 function launchCountdown() {
     tempsRestant = inputCountdown.value;
     compteurInterval = setInterval(() => {
-        if (tempsRestant > 0) {
-            countdownDiv.innerText = tempsRestant;
-            tempsRestant--;
-        } else if (tempsRestant <= 0) {
+        countdownDiv.innerText = tempsRestant;
+        tempsRestant--;
+        if (tempsRestant >= 5) {
+            // audioBip.play();
+            countdownDiv.classList.add("cool");
+            countdownDiv.classList.remove("warning");
+            countdownDiv.classList.remove("danger");
+        } else if (tempsRestant > 2) {
+            audioBip.play();
+            countdownDiv.classList.add("warning");
+            countdownDiv.classList.remove("cool");
+            countdownDiv.classList.remove("danger");
+        } else if (tempsRestant >= 0) {
+            audioBip.play();
+            countdownDiv.classList.remove("cool");
+            countdownDiv.classList.remove("warning");
+            countdownDiv.classList.add("danger");
+        } else if (tempsRestant < 0) {
+            audioBuzzEnd.play();
             clearInterval(compteurInterval);
-            countdownDiv.innerText = "Temps écoulé !"
+            countdownDiv.innerText = "Temps écoulé !";
         }
     }, 1000);
 }
